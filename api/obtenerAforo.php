e164651844834493c4a198d282410c9e24e843de
Jera <jerameelperezm@gmail.com>
2023-10-12 16:51:00 -0500

    ds

--- api/obtenerAforo.php
+++ api/obtenerAforo.php
@@ -3,7 +3,7 @@ require_once 'conexion.php';
 
 try {
     // Realiza la consulta SQL para obtener el valor del contador desde la tabla "Aforo"
-$query = "SELECT \"Contador\" FROM \"Aforo\"";
+$query = 'SELECT "Contador" FROM "Aforo"';
     $result = $db->query($query);
 
     if ($result !== false) {
