007e38493855b28823585a10b43dc47b77f0e9cd
Jera <jerameelperezm@gmail.com>
2023-10-14 14:52:54 -0500

    re

---
+++ conexion.php
@@ -0,0 +1,16 @@
+<?php
+$host = 'db.brieuupydnadfyfdbaes.supabase.co';
+$port = '5432';
+$dbname = 'postgres';
+$user = 'postgres';
+$password = 'nhYxAJ6hX4U9XD9X';
+
+try {
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password";
+    $db = new PDO($dsn);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+} catch (PDOException $e) {
+    echo "Error al conectar a la base de datos x: " . $e->getMessage();
+}
+?>
